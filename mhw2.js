e1834aaf29f5aae9b84a30a0e1ad91f0fc2f66e4
Daniele Famà <116948719+danielefam@users.noreply.github.com>
2024-03-30 14:40:42 +0100

    mhw2.js
    Ordine delle funzioni sistemato rispetto al sito

--- mhw2.js
+++ mhw2.js
@@ -1,3 +1,60 @@
+function cambioImmagineLogoOver(event) {
+    const nuova = document.querySelector('#fisso img');
+    nuova.src = 'immagini/cambiologo.png';
+}
+
+function cambioImmagineLogoOut() {
+    const nuova = document.querySelector('#fisso img');
+    nuova.src = 'https://www.pistacchissimo.it/images/logo-pistacchissimo.png';
+}
+
+
+
+function apriMenu(){
+    const menu = document.querySelector('#blocco-recensioni nav');
+    const tasto = document.querySelector('#tasto-recensioni');
+    tasto.classList.add('hover');
+    menu.classList.remove('hidden');
+}
+
+function chiudiMenu(){
+    const menu = document.querySelector('#blocco-recensioni nav');
+    const tasto = document.querySelector('#tasto-recensioni');
+    tasto.classList.remove('hover');
+    menu.classList.add('hidden');
+}
+
+
+function appendiTesto(event){
+    const divDaModificare = document.querySelector('#main-section-visita');
+    const bloccoScritta = document.createElement('div');
+    const scritta = document.createElement('span');
+    scritta.textContent = "Spero tu abbia comprato qualcosa!";
+
+    divDaModificare.appendChild(bloccoScritta);
+    bloccoScritta.appendChild(scritta);
+    
+    event.currentTarget.removeEventListener('click', appendiTesto);
+}
+
+
+function mostranascondiEpisodi() {
+    const ep = document.querySelector('div#episodi2')    
+    const bottone = document.querySelector('#bottone-episodi span') 
+
+    if(ep.classList.contains('hidden')){
+        ep.classList.remove('hidden');
+        bottone.textContent = 'Nascondi';
+    }
+    else{
+        ep.classList.add('hidden');
+        bottone.textContent = 'Mostra altri episodi';
+    }
+
+}
+
+
+
 function cambiaFooter() {
     const foot = document.querySelector('footer');
     const stileCorrente = document.querySelector('footer em')
@@ -18,60 +75,6 @@ function cambiaFooter() {
             break;
     }
 }
-
-
-function mostranascondiEpisodi() {
-    const ep = document.querySelector('div#episodi2')    
-    const bottone = document.querySelector('#bottone-episodi span') 
-
-    if(ep.classList.contains('hidden')){
-        ep.classList.remove('hidden');
-        bottone.textContent = 'Nascondi';
-    }
-    else{
-        ep.classList.add('hidden');
-        bottone.textContent = 'Mostra altri episodi';
-    }
-
-}
-
-
-function cambioImmagineLogoOver(event) {
-    const nuova = document.querySelector('#fisso img');
-    nuova.src = 'immagini/cambiologo.png';
-}
-
-function cambioImmagineLogoOut() {
-    const nuova = document.querySelector('#fisso img');
-    nuova.src = 'https://www.pistacchissimo.it/images/logo-pistacchissimo.png';
-}
-
-function appendiTesto(event){
-    const divDaModificare = document.querySelector('#main-section-visita');
-    const bloccoScritta = document.createElement('div');
-    const scritta = document.createElement('span');
-    scritta.textContent = "Spero tu abbia comprato qualcosa!";
-
-    divDaModificare.appendChild(bloccoScritta);
-    bloccoScritta.appendChild(scritta);
-    
-    event.currentTarget.removeEventListener('click', appendiTesto);
-}
-
-function apriMenu(){
-    const menu = document.querySelector('#blocco-recensioni nav');
-    const tasto = document.querySelector('#tasto-recensioni');
-    tasto.classList.add('hover');
-    menu.classList.remove('hidden');
-}
-
-function chiudiMenu(){
-    const menu = document.querySelector('#blocco-recensioni nav');
-    const tasto = document.querySelector('#tasto-recensioni');
-    tasto.classList.remove('hover');
-    menu.classList.add('hidden');
-}
-
 
 
 const bottoneCambioFooter = document.querySelector('footer button');
